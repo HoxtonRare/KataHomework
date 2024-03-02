0f64609fef5555759750d0d0be0c7485f0da2e16
Ruslan <ruslan24krut@gmail.com>
2024-03-02 19:04:14 +0300

    edit main

--- task1/src/main/java/homework/Main.java
+++ task1/src/main/java/homework/Main.java
@@ -5,8 +5,10 @@ import lombok.*;
 
 public class Main
 {
-    public void main(String[] args)
+    public static void main(String[] args)
     {
+        Human h = new Human("Danya", "Volkov");
+        System.out.println(h.getFirstName() + " " + h.getLastName());
     }
 }
 
