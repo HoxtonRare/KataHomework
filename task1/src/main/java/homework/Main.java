46ba1cbb9e62af732d6e4f52c904eb24cfe2f8ff
Ruslan <ruslan24krut@gmail.com>
2024-03-02 18:51:02 +0300

    add task1

---
+++ task1/src/main/java/homework/Main.java
@@ -0,0 +1,21 @@
+package homework;
+
+
+import lombok.*;
+
+public class Main
+{
+    public void main(String[] args)
+    {
+    }
+}
+
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+class Human {
+    String firstName;
+    String lastName;
+}
